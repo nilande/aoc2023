1da344dcefefbda53078e88ec318505fefb33375
Anders Nilsson <contact@mg.nilsn.se>
2023-12-07 18:51:47 +0100

    Day 6 solution optimization

--- day 6/day 6.py
+++ day 6/day 6.py
@@ -1,4 +1,4 @@
-import re
+import re, math
 
 #
 # Process input
@@ -15,12 +15,19 @@ distances = list(map(int, re.findall(regex, content, re.MULTILINE)[0].split()))
 #
 # Puzzle 1
 #
+# def race(time, record):
+#     ways_to_beat_record = 0
+#     for press in range(1, time):
+#         distance = (time - press) * press
+#         if distance > record: ways_to_beat_record += 1
+#     return ways_to_beat_record
 def race(time, record):
-    ways_to_beat_record = 0
-    for press in range(1, time):
-        distance = (time - press) * press
-        if distance > record: ways_to_beat_record += 1
-    return ways_to_beat_record
+    # Optimization for the quadratic equation
+    first = (time - math.sqrt(time ** 2 - 4 * record)) / 2
+    first = math.ceil(first) if first != int(first) else int(first) + 1
+    last = (time + math.sqrt(time ** 2 - 4 * record)) / 2
+    last = math.floor(last) if last != int(last) else int(last) - 1
+    return last - first + 1
 
 acc = 1
 for i in range(len(times)):
