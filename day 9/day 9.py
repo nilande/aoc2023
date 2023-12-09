3b6dc8aa5759dfb3439108e5cb019e2ac3456215
Anders Nilsson <contact@mg.nilsn.se>
2023-12-09 11:25:18 +0100

    Day 9  cleanup

--- day 9/day 9.py
+++ day 9/day 9.py
@@ -24,11 +24,7 @@ print(f'Puzzle 1 solution is: {functools.reduce(lambda a, b: a + b, predictions)
 #
 # Puzzle 2
 #
-def back_predict(history):
-    if all(h == 0 for h in history): return 0
-    derivation = [history[i+1] - history[i] for i in range(len(history)-1)]
-    return(history[0]-back_predict(derivation))
-
-back_predictions = map(back_predict, histories)
+# Reverse the lists to predict backwards :-)
+back_predictions = map(predict, [h[::-1] for h in histories])
 
 print(f'Puzzle 2 solution is: {functools.reduce(lambda a, b: a + b, back_predictions)}')
