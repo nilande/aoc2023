8228f7240ba8138246e53bc017356d55eae92354
Anders Nilsson <contact@mg.nilsn.se>
2023-12-14 19:49:19 +0100

    Day 12 puzzle completed

--- day 12/day 12.py
+++ day 12/day 12.py
@@ -1,4 +1,4 @@
-import re, time
+import re, time, functools
 
 def process_record(row):
     cols = row.split()
@@ -18,65 +18,16 @@ def get_validation_masks(broken_record):
                 mask_not_op |= 1
     return (mask_not_op, mask_dmg)
 
-min_transitions_cache = []
-max_transitions_cache = []
-def build_transitions_cache_table(mask_not_op, mask_dmg, length):
-    min_transitions_cache.clear()
-    max_transitions_cache.clear()
-
-    min_transitions = 0
-    prev_value = 0
-    mask_not_op_copy = mask_not_op
-    mask_dmg_copy = mask_dmg
-    i = 0
-    while mask_not_op_copy != 0:
-        if mask_dmg_copy & 1 == 1 and prev_value == 0:
-            prev_value = 1
-            min_transitions += 1
-        elif mask_not_op_copy & 1 == 0 and prev_value == 1:
-            prev_value = 0
-            min_transitions += 1
-        min_transitions_cache.append(min_transitions//2)
-        mask_not_op_copy >>= 1
-        mask_dmg_copy >>= 1
-    while len(min_transitions_cache) < length: min_transitions_cache.append(min_transitions//2)
-
-    max_transitions = 0
-    prev_value = 0
-    mask_not_op_copy = mask_not_op
-    mask_dmg_copy = mask_dmg
-    i = 0
-    while mask_not_op_copy != 0:
-        if mask_not_op_copy & 1 == 1 and prev_value == 0:
-            prev_value = 1
-            max_transitions += 1
-        elif mask_dmg_copy & 1 == 0 and prev_value == 1:
-            prev_value = 0
-            max_transitions += 1
-        max_transitions_cache.append((max_transitions+1)//2)
-        mask_not_op_copy >>= 1
-        mask_dmg_copy >>= 1
-    while len(max_transitions_cache) < length: max_transitions_cache.append((max_transitions+1)//2)
-
-def is_remaining_groups_possible(groups, length):
-    min_transitions = min_transitions_cache[length-1]
-    max_transitions = max_transitions_cache[length-1]
-    if min_transitions <= len(groups) <= max_transitions: return True
-    else: return False
-
-def get_damaged_spring_arrangements(groups, length, mask, mask_not_op, mask_dmg):
-    #if not is_remaining_groups_possible(groups, length): return 0
-
+@functools.cache
+def get_damaged_spring_arrangements(groups, length, mask_not_op, mask_dmg):
     min_shift = sum(groups) + len(groups) - 1
     result = 0
     for i in range(min_shift, length+1): 
-        new_mask = mask | 2**groups[0]-1 << i - groups[0]
-        new_mask_temp = new_mask | 2**(i-groups[0]-1)-1 if len(groups) > 1 else new_mask
-        if new_mask & mask_not_op == new_mask and mask_dmg & new_mask_temp == mask_dmg:
-            if len(groups) > 1:
-                result += get_damaged_spring_arrangements(groups[1:], i-groups[0]-1, new_mask, mask_not_op, mask_dmg)
-            else:
-                result += 1
+        mask = 2**groups[0]-1 << i - groups[0]
+        mask_dmg_temp = mask_dmg & (2**(length-i+groups[0]+1)-1 << i-groups[0]-1) if len(groups) > 1 else mask_dmg & 2**length-1
+        if mask & mask_not_op == mask and mask_dmg_temp & mask == mask_dmg_temp:
+            if len(groups) > 1: result += get_damaged_spring_arrangements(tuple(groups[1:]), i-groups[0]-1, mask_not_op, mask_dmg)
+            else: result += 1
     return result
 
 #
@@ -92,8 +43,7 @@ arrangements = 0
 start_time = time.time()
 for broken_record, contiguous_groups in records:
     mask_not_op, mask_dmg = get_validation_masks(broken_record)
-    build_transitions_cache_table(mask_not_op, mask_dmg, len(broken_record))
-    arrangements += get_damaged_spring_arrangements(contiguous_groups, len(broken_record), 0, mask_not_op, mask_dmg)
+    arrangements += get_damaged_spring_arrangements(tuple(contiguous_groups), len(broken_record), mask_not_op, mask_dmg)
 
 print(f'Puzzle 1 solution is: {arrangements} (in {time.time() - start_time:.3f} seconds)')
 
@@ -103,10 +53,9 @@ print(f'Puzzle 1 solution is: {arrangements} (in {time.time() - start_time:.3f} 
 arrangements = 0
 start_time = time.time()
 for broken_record, contiguous_groups in records:
-    long_broken_record = '?'.join([ broken_record ] * 3)
-    long_contiguous_groups = contiguous_groups * 3
+    long_broken_record = '?'.join([ broken_record ] * 5)
+    long_contiguous_groups = contiguous_groups * 5
     mask_not_op, mask_dmg = get_validation_masks(long_broken_record)
-    build_transitions_cache_table(mask_not_op, mask_dmg, len(long_broken_record))
-    arrangements += get_damaged_spring_arrangements(long_contiguous_groups, len(long_broken_record), 0, mask_not_op, mask_dmg)
+    arrangements += get_damaged_spring_arrangements(tuple(long_contiguous_groups), len(long_broken_record), mask_not_op, mask_dmg)
 
 print(f'Puzzle 2 solution is: {arrangements} (in {time.time() - start_time:.3f} seconds)')
